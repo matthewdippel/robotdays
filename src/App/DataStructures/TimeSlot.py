baac24f3991a470e6e24183fb50c6c411f08f626
matthewdippel <mdippel@ccs.neu.edu>
2016-03-27 17:16:06 -0400

    added properties to the TimeSlot class added empty functions to the TimeSlot class

--- src/App/DataStructures/TimeSlot.py
+++ src/App/DataStructures/TimeSlot.py
@@ -1,4 +1,53 @@
+import datetime
+
 class TimeSlot():
-    def __init__(self):
+    def __init__(self, start, end):
+        self._start = start
+        self._end   = end
+        return
+
+    @property
+    def start(self):
+        return self._start
+
+    @start.setter
+    def start(self, value):
+        self._start = value
+
+    @start.deleter
+    def start(self):
+        del self._start
+
+    @property
+    def end(self):
+        return self._end
+
+    @end.setter
+    def end(self, value):
+        self._end = value
+
+    @end.deleter
+    def end(self):
+        del self._end
+
+    def intersects_internally_with(self, other):
+        """
+        checks if self intersects with other
+        intersection must be beyond endpoints touching
+        :param other: time slot to compare with self
+        :type other: TimeSlot
+        :return: whether or not they intersect
+        :rtype: bool
+        """
         pass
 
+    def intersects_boundary_with(self, other):
+        """
+        checks if self intersects with other
+        intersection can include endpoints touching
+        :param other: time slot to compare with self
+        :type other: TimeSlot
+        :return: whether or not they intersect
+        :rtype: bool
+        """
+        pass
