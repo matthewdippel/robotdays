d4af149f689084aafd4364adc84b44d6ffef2fba
matthewdippel <mdippel@ccs.neu.edu>
2016-05-07 01:54:28 -0400

    implemented add_rule to Task class

---
+++ src/App/Scheduling/Rules/tests/test_rule.py
@@ -0,0 +1,18 @@
+from nose.tools import assert_equal
+from nose.tools import assert_is
+from nose.tools import assert_not_equal
+from nose.tools import assert_raises
+from nose.tools import assert_true
+from nose.tools import assert_false
+
+from datetime import datetime as DT
+
+from robotdays.src.App.DataStructures.TimeSlot import TimeSlot
+from robotdays.src.App.Scheduling.ScheduledTask import ScheduledTask
+from robotdays.src.App.Scheduling.Schedule import Schedule
+from robotdays.src.App.Scheduling.Rules.Rule import Rule
+class test_Rule(object):
+
+    def test_basic_rule_throws_error(self):
+        r = Rule()
+        assert_raises(NotImplementedError, r.satisfied_by, None)
--- src/App/Scheduling/Task.py
+++ src/App/Scheduling/Task.py
@@ -1,6 +1,17 @@
 __author__ = 'mdippel'
 
+from Rules.Rule import Rule
+
 class Task():
     def __init__(self, name):
         self._name = name
-        self.rules = {}
+        self._rules = {}
+
+    def add_rule(self, rule):
+        if not isinstance(rule, Rule):
+            raise ValueError("parameter rule must be of type Rule")
+        rule_type = rule.__class__.__name__
+        if rule_type not in self._rules:
+            self._rules[rule_type] = []
+        self._rules[rule_type].append(rule)
+        return
